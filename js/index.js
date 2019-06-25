6cd68aed0231abc5fa85a8da59907be9f8e35b16
Arlynston <m26416100@jpohn.petra.ac.id>
2019-06-25 16:11:44 +0700

    a

--- js/index.js
+++ js/index.js
@@ -188,7 +188,7 @@ $.getJSON(settings, function(data) {
    html += "<div class='data img-rounded'>";
    html += "<div class='rating'>";
 
-   html += "<span title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
+   html += "<span id='inirating' title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
    html += "  <strong class='text-info'>" + userRating.votes + " votes</strong><br>";
    html += "<button id='fav' style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;' val='"+ value.name +"' daerah='"+ location.locality +"' alamat='"+ location.address +"' cuisines='"+ value.cuisines +"' cost='"+ value.average_cost_for_two +"' thumb='"+ value.thumb +"'><i class='far fa-bookmark'></i></button><br>";
    html += "</div>";
